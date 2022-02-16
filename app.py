3cf6a77b98be8e2f619bf9eada3453605d8758ca
balbino aylagas <balbino_a@hotmail.com>
2022-02-15 21:29:50 -0600

    update 05

--- app.py
+++ app.py
@@ -12,19 +12,18 @@ def home():
     return render_template("home.html", persons=personList)
 
 
-@app.route("/new")
+@app.route("/new", methods=["GET", "POST"])
 def new_person():
-    return render_template("new.html")
-
-
-@app.route("/insert", methods=["POST"])
-def insert_person():
-    if request.method == "POST":
+    if request.method == "GET":
+        return render_template("new.html")
+    elif request.method == "POST":
         name = request.form.get("name")
         age = request.form.get("age")
         salary = request.form.get("salary")
         person = Person(name, age, salary)
         insertNewPerson(person)
+        return redirect(url_for("home"))
+    else:
         return redirect(url_for("home"))
 
 
