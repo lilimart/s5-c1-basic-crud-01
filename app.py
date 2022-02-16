4ff32bb1f4752702159235db1dc6690941ef8f30
balbino aylagas <balbino_a@hotmail.com>
2022-02-15 19:55:14 -0600

    update 03

--- app.py
+++ app.py
@@ -1,11 +1,24 @@
 from flask import Flask, render_template
+from db import selectAllPersons
+
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home():
-    return render_template("home.html")
+    personList = selectAllPersons()
+    return render_template("home.html", persons=personList)
+
+
+@app.route("/new")
+def new_person():
+    return render_template("new.html")
+
+
+@app.route("/insert")
+def insert_person():
+    insertNewPerson()
 
 
 if __name__ == "__main__":
