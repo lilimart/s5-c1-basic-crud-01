88425d11580c6bc20217f2e249844793ecc6ee50
balbino aylagas <balbino_a@hotmail.com>
2022-02-15 20:10:39 -0600

    update 04

---
+++ Person.py
@@ -0,0 +1,9 @@
+class Person:
+    def __init__(self, name, age, salary, id=0) -> None:
+        self.name = name
+        self.age = age
+        self.salary = salary
+        self.id = id
+
+    def __repr__(self) -> str:
+        return f"Person('{self.name}', {self.age}, {self.salary}, {self.id})"
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
-from db import selectAllPersons
+from flask import Flask, render_template, redirect, url_for, request
+from db import selectAllPersons, insertNewPerson
+from Person import Person
 
 
 app = Flask(__name__)
@@ -16,9 +17,15 @@ def new_person():
     return render_template("new.html")
 
 
-@app.route("/insert")
+@app.route("/insert", methods=["POST"])
 def insert_person():
-    insertNewPerson()
+    if request.method == "POST":
+        name = request.form.get("name")
+        age = request.form.get("age")
+        salary = request.form.get("salary")
+        person = Person(name, age, salary)
+        insertNewPerson(person)
+        return redirect(url_for("home"))
 
 
 if __name__ == "__main__":
